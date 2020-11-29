a4e63a50e4ac2b50ccd877ed26a12e54c5ce240f
michaellukeread <mread100@gmail.com>
2020-11-30 01:43:17 +1100

    small changes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <Landing msg="it works!"/>
+    <Landing/>
     <Projects />
   </div>
 </template>
--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
-    <h1>Michael said {{msg}}</h1>
+  <div class='container'>
+    <h1>Hi, I'm Michael Read</h1>
+    <h1>I'm a {{skills[1]}}</h1>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Landing',
-  props: {
-    msg: String
+  data: function () {
+    return {
+      skills: [
+        'Front-end Developer',
+        'UI/UX Designer',
+        'Game Designer'
+      ]
+    }
   }
 }
 </script>
 
 <style scoped>
-
+  .container { height: 100vh; display: flex; justify-content: center; align-items: center; flex-direction: column; }
 </style>
