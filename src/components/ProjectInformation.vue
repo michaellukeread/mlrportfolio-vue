70a89c920d6797816301a0bc90f56de2d9f43972
michaellukeread <mread100@gmail.com>
2020-11-30 01:31:06 +1100

    projects page logic in place

--- src/components/ProjectInformation.vue
+++ src/components/ProjectInformation.vue
@@ -1,8 +1,8 @@
 <template>
-  <div :class='showProjectInformationClass' class='container'>
-      <h1>{{name}}</h1>
-      <p>{{desc}}</p>
-  </div>
+    <div :class='showProjectInformationClass' class='container'>
+        <h1>{{name}}</h1>
+        <p>{{desc}}</p>
+    </div>
 </template>
 
 <script>
@@ -22,6 +22,6 @@ export default {
 </script>
 
 <style scoped>
-    .container { height: 100%; width: 0; background: coral; position: fixed; z-index: 1; top: 0; left: 0; overflow-x: hidden; transition: 0.5s;}
-    .toggleClass { width: 50%; }
+    .container { height: 100%; width: 0; background: coral; position: fixed; z-index: 1; top: 0; left: 0;  transition: 0.5s; overflow: hidden;}
+    .toggleClass { width: 50%; opacity: 1; }
 </style>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,6 +2,8 @@
     <div>
         <div>
             <ProjectInformation :name='selectedProject.name' :desc='selectedProject.desc' :showProjectInformationClass='toggleState'/>
+            <div v-on:click='toggleClass()' :class='{crossOverlay: toggleOverlay}' class='cross'>X</div>
+            <div v-on:click='toggleClass()' :class='{overlayActive: toggleOverlay}' class='overlay'></div>
         </div>
         <ul class='filter'>
             <input type='radio' value='' v-model='search'>
@@ -12,7 +14,7 @@
         <div class='grid'>
             <div class='grid-element' v-for="project in filteredProjects" :key="project.id">
                 <img :src="project.img" :alt="project.id">
-                <input type='radio' :value='project' v-model='selectedProject' v-on:click="toggleOnClass()">
+                <input type='radio' :value='project' v-model='selectedProject' v-on:click="toggleClass()">
             </div>
         </div>
     </div>
@@ -33,12 +35,14 @@ export default {
             projects: jsonProjects,
             search: '',
             selectedProject: '',
-            toggleState: {toggleClass: false}
+            toggleState: {toggleClass: false},
+            toggleOverlay: false
         }
     },
     methods: {
-        toggleOnClass() {
+        toggleClass() {
             this.toggleState.toggleClass = !this.toggleState.toggleClass;
+            this.toggleOverlay = !this.toggleOverlay;
         }
     },    
     computed: {
@@ -57,5 +61,9 @@ export default {
     .grid { display: grid; grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); grid-template-rows: auto; grid-auto-flow: row dense;}
     .grid img { width: 100%; height: 100%; object-fit: cover; transition: 0.3s; text-align: center; color: white; }
     .grid img:hover { opacity: 20%; cursor: pointer; }
-    
+    .cross { position: fixed; top: 2%; left: 1%; z-index: 1; transition: 0.8s; opacity: 0;}
+    .crossOverlay { opacity: 1; }
+    .overlay { opacity: 0; background-color: black; width: 100vw; height: 100vh; position: fixed; top: 0; left: 0; z-index: -1; transition: 0.8s;}
+    .overlayActive { opacity: 0.2; z-index: 0;}
+
 </style>
