5d95daf975f85ac9b47e5a57d40c74b0734e28f0
michaellukeread <mread100@gmail.com>
2020-11-29 18:40:24 +1100

    made some basic changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Landing msg="it works!"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Landing from './components/Landing.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Landing
   }
 }
 </script>
---
+++ src/components/Landing.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Michael said {{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Landing',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
